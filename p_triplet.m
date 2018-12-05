59b6a60299b57f408384756630d24eab5a04d468
Jackson Forner <jef170030@utdallas.edu>
2018-12-05 17:20:57 -0600

    worked on ProjectEuler functions

--- p_triplet.m
+++ p_triplet.m
@@ -1,4 +1,4 @@
-function p = p_triplet(a, b, c)
+function [x, y, z, p] = p_triplet(a, b, c)
     % p_triplet: computes the product abc of the pythagorean triplet (a, b, c)
     %     Signature: p = p_triplet(a, b, c)
     %     Input a: a natural
@@ -6,7 +6,7 @@ function p = p_triplet(a, b, c)
     %     Input c: a natural, satisfying a^2 + b^2 = c^2
     %     Output p: the product abc
     
-    a = a;
-    b = b;
-    c = c;
+    assert(a < b && b < c, 'correct ordering of elements', 'a must be less than b, which must be less than c')
+    
+    p =a*b*c;
 end
