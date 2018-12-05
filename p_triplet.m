fd5199a86189a4acc9416d041e4d293144cd3d35
Jackson Forner <jef170030@utdallas.edu>
2018-12-05 16:52:08 -0600

    p_triplet function

---
+++ p_triplet.m
@@ -0,0 +1,12 @@
+function p = p_triplet(a, b, c)
+    % p_triplet: computes the product abc of the pythagorean triplet (a, b, c)
+    %     Signature: p = p_triplet(a, b, c)
+    %     Input a: a natural
+    %     Input b: a natural
+    %     Input c: a natural, satisfying a^2 + b^2 = c^2
+    %     Output p: the product abc
+    
+    a = a;
+    b = b;
+    c = c;
+end
